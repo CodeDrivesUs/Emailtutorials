83d9bbd08489c00401fd1bc8c253f6122e71e6d0
mzweshiaih <42660351+mzweshiaih@users.noreply.github.com>
2019-10-21 22:04:09 +0200

    co

---
+++ Email/Controllers/GmailController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Net;
+using Email.Models;
+using System.Web.Mvc;
+
+namespace Email.Controllers
+{
+    public class GmailController : Controller
+    {
+        // GET: Gmail
+        public ActionResult Send()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Send(Gmail gmail)
+        {
+            gmail.SendMail();
+            return View();
+        }
+    }
+}
---
+++ Email/Models/Gmail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace Email.Models
+{
+    public class Gmail
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        public void SendMail()
+        {
+            MailMessage mc = new MailMessage(System.Configuration.ConfigurationManager.AppSettings["Email"].ToString(), To);
+            mc.Subject = Subject;
+            mc.Body = Body;
+            mc.IsBodyHtml = false;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Timeout = 1000000;
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            NetworkCredential nc = new NetworkCredential(System.Configuration.ConfigurationManager.AppSettings["Email"].ToString(), System.Configuration.ConfigurationManager.AppSettings["Password"].ToString());
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = nc;
+            smtp.Send(mc);
+        }
+    }
+}
